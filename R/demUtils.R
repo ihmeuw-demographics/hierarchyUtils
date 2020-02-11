5bd8f735beb5be4f1dd0e95ba4b7dd4171747692
Charlton Callender <chacalle@uw.edu>
2020-02-10 17:24:27 -0800

    add package documentation and update package title and description

---
+++ R/demUtils.R
@@ -0,0 +1,13 @@
+#' demUtils: A package with utility functions for common demographics data and estimation problems
+#'
+#' The demUtils package provides two categories of important functions:
+#' aggregation and scaling.
+#'
+#' @section aggregate functions:
+#' The aggregate functions aggregate counts of events by sex, age and/or location.
+#' @section scale functions:
+#' The scale functions scale counts by sex, age and/or location to an aggregate.
+#'
+#' @docType package
+#' @name demUtils
+NULL
