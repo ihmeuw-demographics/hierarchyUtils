2eb2ff15599b28e7bda81e80dc520c4c5dd5d4d1
Katie Paulson <krpaul@uw.edu>
2020-04-28 09:55:23 -0700

    remove scale, incorporate feedback on logit functions

--- R/logit_utils.R
+++ R/logit_utils.R
@@ -1,12 +1,10 @@
 #' @title Logit and inverse logit functions
 #'
-#' @description Logit and inverse logit functions, with options to scale input
-#'   to logit, scale output of inverse logit, or perform a transformation
-#'   such that the domain in non-logit space is as specified (standard
-#'   is (0, 1)).
+#' @description Logit and inverse logit functions, with option to perform a
+#'   transformation such that the domain in non-logit space is as specified
+#'   (standard is (0, 1)).
 #'
 #' @param x \[`numeric()`\]\cr Value to take the logit or inverse logit of.
-#' @param scale \[`numeric()`\]\cr Value to scale by. Default 1.
 #' @param domain_lower \[`numeric()`\]\cr Lower bound of domain in non-logit
 #'   space, inclusive. Default 0. Logit at lower bound is -Inf.
 #' @param domain_upper \[`numeric()`\]\cr Upper bound of domain in non-logit
@@ -20,28 +18,18 @@
 #' * logit (x) = log (x / (1-x))
 #' * inverse logit (x) = exp (x) / (1 + exp (x))
 #'
-#' Optional scaled logit with 'scale' argument:
-#'
-#' * logit (x, s) = log (s * x / (1 - s * x))
-#' * inverse logit (x, s) = (1 / s) * exp (x) / (1 + exp (x))
-#'
 #' Optional logit with transformed domain:
 #'
-#' * logit (x, l, u) = log(x' / (1 - x')) where x' = (x - l) / (u - l)
-#' * inverse logit (x, l, u) = [ exp (x) / (1 + exp (x)) ] * (u - l) + l
+#' * logit (x, l, u) = log (x' / (1 - x')) where x' = (x - l) / (u - l)
+#' * inverse logit (x, l, u) = ( exp (x) / (1 + exp (x)) ) * (u - l) + l
 #'
-#' Other notes: Scaled values of x outside of (`domain_lower`, `domain_upper`)
+#' Other notes: Values of x outside of (`domain_lower`, `domain_upper`)
 #'   will return NaN and result in a warning from `logit` function.
 #'
 #' @examples
 #' # Standard
 #' logit(0.1)
 #' invlogit(-2)
-#'
-#' # Scaled
-#' x <- c(10, 20, 30)
-#' logit_x <- logit(x, scale = 0.01)
-#' x_again <- invlogit(logit_x, scale = 0.01)
 #'
 #' # Domain shift
 #' x <- stats::runif(n = 100, min = 10, max = 20)
@@ -50,16 +38,15 @@
 #'
 #' @name logit_utils
 
+
 # ===================================================================
 #' @rdname logit_utils
 #' @export
-logit <- function(x, scale = 1, domain_lower = 0, domain_upper = 1) {
+logit <- function(x, domain_lower = 0, domain_upper = 1) {
 
-  assertive::assert_is_numeric(x)
-  assertive::assert_is_numeric(scale)
-  assertthat::assert_that(domain_lower < domain_upper)
+  check_logit_inputs(x, domain_lower, domain_upper)
 
-  x <- (scale * x - domain_lower) / (domain_upper - domain_lower)
+  x <- (x - domain_lower) / (domain_upper - domain_lower)
   result <- log(x / (1 - x))
 
   return(result)
@@ -69,15 +56,26 @@ logit <- function(x, scale = 1, domain_lower = 0, domain_upper = 1) {
 # ===================================================================
 #' @rdname logit_utils
 #' @export
-invlogit <- function(x, scale = 1, domain_lower = 0, domain_upper = 1) {
+invlogit <- function(x, domain_lower = 0, domain_upper = 1) {
 
-  assertive::assert_is_numeric(x)
-  assertive::assert_is_numeric(scale)
-  assertthat::assert_that(domain_lower < domain_upper)
+  check_logit_inputs(x, domain_lower, domain_upper)
 
   result <- exp(x) / (1 + exp(x))
   result <- result * (domain_upper - domain_lower) + domain_lower
-  result <- (1 / scale) * result
 
   return(result)
+}
+
+
+# ===================================================================
+#' @rdname logit_utils
+check_logit_inputs <- function(x, domain_lower, domain_upper) {
+
+  # check numeric, input lengths, and lower < upper
+  assertive::assert_is_numeric(c(x, domain_lower, domain_upper))
+  assertthat::assert_that(length(domain_lower) %in% c(1, length(x)) &
+                            length(domain_upper) %in% c(1, length(x)),
+                          msg = "`domain_lower` and `domain_upper` must be
+                                  length 1 or same length as `x`.")
+  assertthat::assert_that(all(domain_lower < domain_upper))
 }
--- tests/testthat/test-logit_utils.R
+++ tests/testthat/test-logit_utils.R
@@ -8,16 +8,25 @@ testthat::test_that("Test that `logit` and `invlogit` inverse works", {
   # standard
   testthat::expect_equal(invlogit(logit(0.1)), 0.1)
 
-  # scaled
-  x <- c(10, 20, 30)
-  logit_x <- logit(x, scale = 0.01)
-  x_again <- invlogit(logit_x, scale = 0.01)
-  testthat::expect_equal(x, x_again)
-
   # domain shift
-  x <- runif(n = 100, min = 10, max = 20)
+  x <- runif(n = 5, min = 10, max = 20)
   logit_x <- logit(x, domain_lower = 10, domain_upper = 20)
   x_again <- invlogit(logit_x, domain_lower = 10, domain_upper = 20)
+  testthat::expect_equal(x, x_again)
+
+})
+
+testthat::test_that("Test that `logit` works with vector input", {
+
+  # standard
+  testthat::expect_equal(invlogit(logit(x = c(0.1, 0.2))), c(0.1, 0.2))
+
+  # domain shift
+  x <- c(runif(n = 5, min = 10, max = 20), runif(n = 5, min = 20, max = 30))
+  domain_lower = c(rep(10, 5), rep(20, 5))
+  domain_upper = c(rep(20, 5), rep(30, 5))
+  logit_x <- logit(x, domain_lower, domain_upper)
+  x_again <- invlogit(logit_x, domain_lower, domain_upper)
   testthat::expect_equal(x, x_again)
 
 })
